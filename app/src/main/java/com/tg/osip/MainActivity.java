1b77a4f20d85ddbf70426407e2c03f48eab3064b
Eugene Matsyuk <e.matsyuk@lingualeo.com>
2015-09-11 13:17:42 +0300

    - added RxAndroid - updated tdlibs

--- app/src/main/java/com/tg/osip/MainActivity.java
+++ app/src/main/java/com/tg/osip/MainActivity.java
@@ -2,8 +2,15 @@ package com.tg.osip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.tg.osip.utils.AndroidUtils;
+
+import org.drinkless.td.libcore.telegram.Client;
+import org.drinkless.td.libcore.telegram.TG;
+import org.drinkless.td.libcore.telegram.TdApi;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +18,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        TG.setDir(AndroidUtils.getCacheDirPath());
+        TG.setUpdatesHandler(updatesHandler);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        TG.getClientInstance().send(new TdApi.GetAuthState(), authGetResultHandler);
+
     }
 
+    private Client.ResultHandler updatesHandler = new Client.ResultHandler() {
+        @Override
+        public void onResult(TdApi.TLObject object) {
+            Log.d("update:", object.toString());
+        }
+    };
+
+    private Client.ResultHandler authGetResultHandler = new Client.ResultHandler() {
+        @Override
+        public void onResult(TdApi.TLObject object) {
+            Log.d("auth:", object.toString());
+        }
+    };
 }
