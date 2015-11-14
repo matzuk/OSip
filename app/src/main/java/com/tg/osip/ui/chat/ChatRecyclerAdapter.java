f9980903427b6d7565c146d3a12b4286235a13ff
Eugene Matsyuk <matzuk2@mail.ru>
2015-11-14 14:09:41 +0300

    - added Actions ti Chat screen

--- app/src/main/java/com/tg/osip/ui/chat/ChatRecyclerAdapter.java
+++ app/src/main/java/com/tg/osip/ui/chat/ChatRecyclerAdapter.java
@@ -29,6 +29,7 @@ public class ChatRecyclerAdapter extends AutoLoadingRecyclerViewAdapter<TdApi.Me
     private static final int TEMP_SEND_STATE_IS_ERROR = 0;
 
     private static final int MAIN_VIEW = 0;
+    private static final int ACTION_VIEW = 3;
     private static final int UNSUPPORTED_VIEW = 4;
 
     private int myUserId;
@@ -50,6 +51,18 @@ public class ChatRecyclerAdapter extends AutoLoadingRecyclerViewAdapter<TdApi.Me
             chatMessageText = (TextView) itemView.findViewById(R.id.chat_message_text);
             chatMessageSendingTime = (TextView) itemView.findViewById(R.id.chat_message_sending_time);
             chatMessageUnreadOutbox = (ImageView) itemView.findViewById(R.id.chat_message_unread_outbox);
+        }
+    }
+
+    static class ActionViewHolder extends RecyclerView.ViewHolder {
+
+        TextView chatMessageText;
+        ImageView photo;
+
+        public ActionViewHolder(View itemView) {
+            super(itemView);
+            chatMessageText = (TextView) itemView.findViewById(R.id.chat_message_text);
+            photo = (ImageView) itemView.findViewById(R.id.photo);
         }
     }
 
@@ -79,7 +92,11 @@ public class ChatRecyclerAdapter extends AutoLoadingRecyclerViewAdapter<TdApi.Me
         if (viewType == MAIN_VIEW) {
             View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_chat_list_message_text, parent, false);
             return new MainViewHolder(v);
-        } else if (viewType == UNSUPPORTED_VIEW) {
+        } else if (viewType == ACTION_VIEW) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_chat_list_message_actions, parent, false);
+            return new ActionViewHolder(v);
+        }
+        else if (viewType == UNSUPPORTED_VIEW) {
             View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_chat_list_message_unsupport, parent, false);
             return new UnsupportedViewHolder(v);
         }
@@ -90,6 +107,12 @@ public class ChatRecyclerAdapter extends AutoLoadingRecyclerViewAdapter<TdApi.Me
     public int getItemViewType(int position) {
         if (getItem(position).message.getClass() == TdApi.MessageText.class) {
             return MAIN_VIEW;
+        } else if (getItem(position).message.getClass() == TdApi.MessageChatAddParticipant.class ||
+                getItem(position).message.getClass() == TdApi.MessageChatChangePhoto.class ||
+                getItem(position).message.getClass() == TdApi.MessageChatChangeTitle.class ||
+                getItem(position).message.getClass() == TdApi.MessageChatDeleteParticipant.class ||
+                getItem(position).message.getClass() == TdApi.MessageChatDeletePhoto.class) {
+            return ACTION_VIEW;
         }
         return UNSUPPORTED_VIEW;
     }
@@ -99,6 +122,9 @@ public class ChatRecyclerAdapter extends AutoLoadingRecyclerViewAdapter<TdApi.Me
         switch (getItemViewType(position)) {
             case MAIN_VIEW:
                 onBindTextHolder(holder, position);
+                break;
+            case ACTION_VIEW:
+                onBindActionHolder(holder, position);
                 break;
             case UNSUPPORTED_VIEW:
                 onBindUnsupportedHolder(holder, position);
@@ -155,6 +181,65 @@ public class ChatRecyclerAdapter extends AutoLoadingRecyclerViewAdapter<TdApi.Me
         }
     }
 
+    private void onBindActionHolder(RecyclerView.ViewHolder holder, int position) {
+        ActionViewHolder actionHolder = (ActionViewHolder) holder;
+        TdApi.Message message = getItem(position);
+        if (message == null) {
+            return;
+        }
+        TdApi.MessageContent messageContent = message.message;
+        if (messageContent == null) {
+            return;
+        }
+        TdApi.User userFrom = usersMap.get(message.fromId).getUser();
+        String actionText = getActionText(messageContent, userFrom);
+        if (actionText != null) {
+            actionHolder.chatMessageText.setText(actionText);
+        }
+        if (messageContent.getClass() == TdApi.MessageChatChangePhoto.class) {
+            TdApi.Photo photo = ((TdApi.MessageChatChangePhoto)messageContent).photo;
+            actionHolder.photo.setVisibility(View.VISIBLE);
+            // TODO add later
+            //setPhoto(actionHolder.photo, photo, GROUP_SMALL_PHOTO, true);
+        } else {
+            actionHolder.photo.setImageDrawable(null);
+            actionHolder.photo.setVisibility(View.GONE);
+        }
+    }
+
+    // TODO add name selecting
+    private String getActionText(TdApi.MessageContent messageContent, TdApi.User userFrom) {
+        String text;
+        if (messageContent.getClass() == TdApi.MessageChatAddParticipant.class) {
+            text = userFrom.firstName + " " + userFrom.lastName;
+            text = text + " " + ApplicationSIP.applicationContext.getString(R.string.invited);
+            text = text + " " +((TdApi.MessageChatAddParticipant)messageContent).user.firstName + " " + ((TdApi.MessageChatAddParticipant)messageContent).user.lastName;
+            return text;
+        } else if (messageContent.getClass() == TdApi.MessageChatChangePhoto.class) {
+            text = userFrom.firstName + " " + userFrom.lastName;
+            text = text + " " + ApplicationSIP.applicationContext.getString(R.string.changed_group_photo);
+            return text;
+        } else if (messageContent.getClass() == TdApi.MessageChatChangeTitle.class) {
+            text = userFrom.firstName + " " + userFrom.lastName;
+            text = text + " " + ApplicationSIP.applicationContext.getString(R.string.changed_group_name);
+            text = text + " " +((TdApi.MessageChatChangeTitle)messageContent).title;
+            return text;
+        } else if (messageContent.getClass() == TdApi.MessageChatDeleteParticipant.class) {
+            text = ((TdApi.MessageChatDeleteParticipant)messageContent).user.firstName + " " + ((TdApi.MessageChatDeleteParticipant)messageContent).user.lastName;
+            if (userFrom != null) {
+                text = userFrom.firstName + " " + userFrom.lastName + ApplicationSIP.applicationContext.getString(R.string.removed) + " " + text;
+            } else {
+                text = text + " " + ApplicationSIP.applicationContext.getString(R.string.left_group);
+            }
+            return text;
+        } else if (messageContent instanceof TdApi.MessageChatDeletePhoto) {
+            text = userFrom.firstName + " " + userFrom.lastName;
+            text = text + " " + ApplicationSIP.applicationContext.getString(R.string.removed_group_photo);
+            return text;
+        }
+        return "";
+    }
+
     private void onBindUnsupportedHolder(RecyclerView.ViewHolder holder, int position) {
         UnsupportedViewHolder unsupportedHolder = (UnsupportedViewHolder) holder;
         TdApi.Message message = getItem(position);
@@ -180,10 +265,6 @@ public class ChatRecyclerAdapter extends AutoLoadingRecyclerViewAdapter<TdApi.Me
 
     public void setMyUserId(int myUserId) {
         this.myUserId = myUserId;
-    }
-
-    public int getMyUserId() {
-        return myUserId;
     }
 
     public void setLastChatReadOutboxId(int lastChatReadOutboxId) {
