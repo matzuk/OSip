87d33eb316e0e9052473b916091d914bcb617f00
Eugene Matsyuk <matzuk2@mail.ru>
2015-11-20 06:43:09 +0300

    added RxJavaPlugins

--- app/src/main/java/com/tg/osip/ApplicationSIP.java
+++ app/src/main/java/com/tg/osip/ApplicationSIP.java
@@ -2,8 +2,13 @@ package com.tg.osip;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.util.Log;
 
 import com.tg.osip.utils.log.Logger;
+
+import rx.plugins.RxJavaErrorHandler;
+import rx.plugins.RxJavaPlugins;
 
 /**
  * @author e.matsyuk
@@ -18,6 +23,20 @@ public class ApplicationSIP extends Application {
         super.onCreate();
         applicationContext = getApplicationContext();
         Logger.registerLogger(this);
+
+        if (isDebugMode()) {
+            // Determining unchecked exceptions in Subscribers
+            RxJavaPlugins.getInstance().registerErrorHandler(new RxJavaErrorHandler() {
+                @Override
+                public void handleError(Throwable e) {
+                    Log.w("Error", e);
+                }
+            });
+        }
+    }
+
+    public boolean isDebugMode() {
+        return (0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE));
     }
 
 }
